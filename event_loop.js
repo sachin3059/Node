c6cc72dd15903890420072c276475df664166b7f
sachin3059 <sachin54077kumar@gmail.com>
2025-01-23 17:45:42 +0530

    corrected Promise.resolve line code

--- event_loop.js
+++ event_loop.js
@@ -3,7 +3,7 @@ const a = 100;
 
 setImmediate (() => console.log("set Immediate"));
 
-Promise.resolve(() => console.log("Promise resolve"));
+Promise.resolve("Promise").then(console.log);
 
 fs.readFile("./README.md", "utf-8", () => console.log("file reading cb"));
 
